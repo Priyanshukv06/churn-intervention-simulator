1bf39a6254797f9147583d3e601564d72f3e0331
Priyanshukv06 <priyanshukv06@gmail.com>
2026-03-24 09:53:45 +0530

    Fix ModuleNotFoundError: add setup.py and -e . for cloud path resolution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="churn_simulator",
-    version="0.1.0",
+    name="churn-intervention-simulator",
+    version="1.0.0",
     packages=find_packages(),
 )
